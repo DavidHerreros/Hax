32394d4cd8303747c39bd485881e728dc491cf23
DavidHerreros <david.herreroscalero@gmail.com>
2025-10-21 21:00:42 +0200

    Refactor loading to ram or mmap

--- hax/generators/generator_metadata.py
+++ hax/generators/generator_metadata.py
@@ -33,32 +33,68 @@ class MetaDataGenerator:
         else:
             return self.md.getMetaDataImage(idx)[..., None], idx
 
+    def load_images_to_ram(self, images_order=None):
+        def build_ram_slab(N, H, W, batch_size=4096, threads=16):
+            X = np.empty((N, H, W), dtype=np.float32)
+
+            def _ranges():
+                for s in range(0, N, batch_size):
+                    e = min(s + batch_size, N)
+                    yield s, e, np.arange(s, e, dtype=np.int64)
+
+            with ThreadPoolExecutor(max_workers=threads) as ex:
+                futs = []
+                for s, e, idx in _ranges():
+                    futs.append((s, e, ex.submit(self.md.getMetaDataImage, idx)))
+                with tqdm(total=N, file=sys.stdout, ascii=" >=", colour="green") as pbar:
+                    for s, e, fut in futs:
+                        X[s:e] = fut.result()
+                        pbar.update(e - s)
+            return X
+
+        if images_order is None:
+            images_order = np.arange(len(self.md))
+
+        print(f"{bcolors.OKCYAN}\n###### Loading images to RAM... ######")
+        H, W = self.md.getMetaDataImage(0).shape
+        images = build_ram_slab(len(self.md), H, W, batch_size=4096, threads=8)[..., None]
+        return images[images_order]
+
+    def load_images_to_mmap(self, mmap_output_dir=None, images_order=None):
+        from mmap_ninja import numpy as np_ninja
+
+        if mmap_output_dir is None:
+            mmap_output_dir = os.path.join(os.path.dirname(self.file), "images_mmap")
+        else:
+            mmap_output_dir = os.path.join(mmap_output_dir, "images_mmap")
+
+        if images_order is None:
+            images_order = np.arange(len(self.md))
+
+        if not os.path.isdir(mmap_output_dir):
+            print(f"{bcolors.OKCYAN}\n###### Creating MMAP from images... ######")
+            np_ninja.from_generator(
+                out_dir=mmap_output_dir,
+                sample_generator=map(self.md.getMetaDataImage, images_order),
+                batch_size=1024,
+                verbose=True
+            )
+        return np_ninja.open_existing(mmap_output_dir)
+
     def return_tf_dataset(self, preShuffle=False, shuffle=True, prefetch=-1, batch_size=8, mmap=True, mmap_output_dir=None):
         import tensorflow_datasets as tfds
         import tensorflow as tf
+
         tf.config.set_visible_devices([], device_type='GPU')
         with tf.device("/CPU:0"):
             file_idx = np.arange(len(self.md))
+
             if preShuffle:
                 np.random.shuffle(file_idx)
+
             if mmap:
                 drop_reminder = True
-                from mmap_ninja import numpy as np_ninja
-
-                if mmap_output_dir is None:
-                    mmap_output_dir = os.path.join(os.path.dirname(self.file), "images_mmap")
-                else:
-                    mmap_output_dir = os.path.join(mmap_output_dir, "images_mmap")
-
-                if not os.path.isdir(mmap_output_dir):
-                    print(f"{bcolors.OKCYAN}\n###### Creating MMAP from images... ######")
-                    np_ninja.from_generator(
-                        out_dir=mmap_output_dir,
-                        sample_generator=map(self.md.getMetaDataImage, file_idx),
-                        batch_size=1024,
-                        verbose=True
-                    )
-                images = np_ninja.open_existing(mmap_output_dir)
+                images = self.load_images_to_mmap(mmap_output_dir=mmap_output_dir, images_order=None)
 
                 def _load_image(idx):
                     idx = idx.astype(np.int64)
@@ -86,52 +122,52 @@ class MetaDataGenerator:
                 dataset = tf.data.Dataset.from_tensor_slices(file_idx)
             else:
                 drop_reminder = False
+                images = self.load_images_to_ram(images_order=file_idx)
 
-                def build_ram_slab(N, H, W, batch_size=4096, threads=16):
-                    X = np.empty((N, H, W), dtype=np.float32)
-
-                    def _ranges():
-                        for s in range(0, N, batch_size):
-                            e = min(s + batch_size, N)
-                            yield s, e, np.arange(s, e, dtype=np.int64)
-
-                    with ThreadPoolExecutor(max_workers=threads) as ex:
-                        futs = []
-                        for s, e, idx in _ranges():
-                            futs.append((s, e, ex.submit(self.md.getMetaDataImage, idx)))
-                        with tqdm(total=N, file=sys.stdout, ascii=" >=", colour="green") as pbar:
-                            for s, e, fut in futs:
-                                X[s:e] = fut.result()
-                                pbar.update(e - s)
-                    return X
-
-                print(f"{bcolors.OKCYAN}\n###### Loading images to RAM... ######")
-                H, W = self.md.getMetaDataImage(0).shape
-                images = build_ram_slab(len(self.md), H, W, batch_size=4096, threads=8)[..., None]
-                images = images[file_idx]  # Reorder according to preShuffle
                 if self.mode == "tomo":
                     subtomo_labels = self.sinusoid_table[self.md[file_idx, "subtomo_labels"].astype(int) - 1]
                     dataset = tf.data.Dataset.from_tensor_slices(((images, subtomo_labels), (file_idx, file_idx)))
                 else:
                     dataset = tf.data.Dataset.from_tensor_slices((images, file_idx))
+
             if shuffle:
                 dataset = dataset.shuffle(len(file_idx))
+
             if prefetch == -1:
                 prefetch = tf.data.AUTOTUNE
+
             dataset = dataset.batch(batch_size, drop_remainder=drop_reminder)
+
             if mmap:
                 dataset = dataset.map(map_fn, num_parallel_calls=tf.data.AUTOTUNE, deterministic=False)
+
             return tfds.as_numpy(dataset.prefetch(prefetch))
 
-    def return_torch_dataset(self, shuffle=True, batch_size=8):
-        from torch.utils.data import default_collate, DataLoader
+    def return_torch_dataset(self, shuffle=True, preShuffle=True, batch_size=8, mmap=True, mmap_output_dir=None):
+        import torch
+        from torch.utils.data import default_collate, DataLoader, TensorDataset
+
+        file_idx = np.arange(len(self.md))
+
+        if preShuffle:
+            np.random.shuffle(file_idx)
+
+        if mmap:
+            drop_last = True
+            images = self.load_images_to_mmap(mmap_output_dir=mmap_output_dir, images_order=file_idx)
+        else:
+            drop_last = False
+            images = self.load_images_to_ram(images_order=file_idx)
+
+        dataset = TensorDataset(torch.from_numpy(images), torch.from_numpy(file_idx))
 
         def numpy_collate(batch):
             return tree_map(np.asarray, default_collate(batch))
 
-        return DataLoader(self, batch_size=batch_size, shuffle=shuffle, num_workers=0, collate_fn=numpy_collate)
+        return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=0, collate_fn=numpy_collate,
+                          drop_last=drop_last)
 
-    def return_grain_dataset(self, preShuffle=False, shuffle=True, batch_size=8):
+    def return_grain_dataset(self, preShuffle=False, shuffle=True, batch_size=8, mmap=True, mmap_output_dir=None):
         import grain
 
         class CustomSource(grain.sources.RandomAccessDataSource):
@@ -152,16 +188,24 @@ class MetaDataGenerator:
         file_idx = np.arange(len(self.md))
         if preShuffle:
             np.random.shuffle(file_idx)
-        images = self.md.getMetaDataImage(file_idx)[..., None]
+
+        if mmap:
+            drop_remainder = True
+            images = self.load_images_to_mmap(mmap_output_dir=mmap_output_dir, images_order=file_idx)
+        else:
+            drop_remainder = False
+            images = self.load_images_to_ram(images_order=file_idx)
+
         if self.mode == "tomo":
             subtomo_labels = self.sinusoid_table[self.md[file_idx, "subtomo_labels"].astype(int) - 1]
             dataset = grain.MapDataset.source(CustomSource(images, file_idx, subtomo_labels))
         else:
             dataset = grain.MapDataset.source(CustomSource(images, file_idx))
+
         if shuffle:
             seed = random.randint(0, 2 ** 32 - 1)
             dataset = dataset.shuffle(seed=seed)
-        return dataset.to_iter_dataset().batch(batch_size)
+        return dataset.to_iter_dataset().batch(batch_size, drop_remainder=drop_remainder)
 
 def extract_columns(md, hasCTF=None):
     hasCTF = md.isMetaDataLabel("ctfDefocusU") if hasCTF is None else hasCTF
